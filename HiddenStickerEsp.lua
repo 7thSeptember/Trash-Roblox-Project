7f8ba6382cd3364aad9ddda6421972c22cb6fd8a
7thSeptember <hardrpass@gmail.com>
2024-08-09 15:03:55 +0700

    Update HiddenStickerEsp.lua
    Added some configurable variable

--- HiddenStickerEsp.lua
+++ HiddenStickerEsp.lua
@@ -4,8 +4,8 @@ H = workspace:FindFirstChild("HiddenStickers")
 if H == false then print("No folder found") return end
 for _,v in pairs(H:GetChildren()) do
    local D = Drawing.new("Square")
-   D.Color = Color3.new(0.5,0.5,05.)
-   D.Thickness = 0.8
+   D.Color = _G.Color or Color3.new(0.5,0.5,05.)
+   D.Thickness = _G.Thickness or 0.8
    coroutine.wrap(function()
    game:GetService("RunService").RenderStepped:Connect(function()
       if (v.Parent ~= H) then 
@@ -15,7 +15,7 @@ for _,v in pairs(H:GetChildren()) do
       local V, On = workspace.CurrentCamera:WorldToViewportPoint(v.Position)
       if On then
          D.Visible = true
-         D.Size = Vector2.new(20,20)
+         D.Size = _G.Size or Vector2.new(20,20)
          D.Position = Vector2.new(V.X,V.Y)
       else
          D.Visible = false
