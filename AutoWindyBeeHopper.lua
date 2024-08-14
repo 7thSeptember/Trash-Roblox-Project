3de9153335ffb8ac65ccb19e8bfd9b8aab0c5f45
7thSeptember <hardrpass@gmail.com>
2024-08-14 19:38:33 +0700

    Update AutoWindyBeeHopper.lua
    added esp I guess

--- AutoWindyBeeHopper.lua
+++ AutoWindyBeeHopper.lua
@@ -26,11 +26,30 @@ local function hop()
         end
     end
 end
+local rs = game:GetService("RunService")
+local c = game.CurrentCamera
+function live(o)
+    local Square = Drawing.new("Square")
+    Square.Thickness = 0.5
+    Square.Size = _G.WSize or Vector2.new(20,20)
+    RS.RenderStepped:connect(function()
+       local V, On = c:WorldToViewportPoint(o.Position)
+       if On then
+           Square.Visible = true
+           Square.Position = V
+       else
+           Square.Visible = false
+       end
+    end)
+end
 
 workspace.NPCBees.ChildAdded:Connect(function(child) --credits to trans for this
     task.wait(0.33)
     if child.Name == "Windy" then
         foundNPC = true
+        local S,F = pcall(function()
+        live(workspace.NPCBees.Windy)
+        end)
     end
 end)
 
